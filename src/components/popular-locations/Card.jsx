6320d2347a615b3c30ad86942a2c7253c06c0f9e
Ayon <suprovatbit@gmail.com>
2025-03-20 11:04:46 +0600

    Popular-Locations-Com

---
+++ src/components/popular-locations/Card.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Card = ({ place }) => {
+  return (
+    <Link
+      href="/catalog"
+      className="cursor-pointer h-[500px] w-[350px] flex flex-wrap rounded-xl shadow-md"
+    >
+      <div className="relative h-2/3 w-full">
+        <Image
+          src={place.image}
+          alt="place-location-imgs"
+          className="h-full w-full overflow-hidden rounded-tl-xl rounded-tr-xl object-cover"
+        />
+        <div className="absolute right-0 bottom-0 capitalize p-4 bg-blue-700 text-white rounded-tl-xl font-semibold">
+          {place.city}
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 p-4">
+        <h2 className="text-center text-2xl text-slate-800 font-semibold">
+          {place.numOfPlaces} Places to stay
+        </h2>
+        <p className="text-center mt-2 text-lg text-slate-700">
+          Discover the best hotel or apartment for your adventurous journey.
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
