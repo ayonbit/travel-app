7182cc8fe06d3a0c669f8d6b27e89422ccdba8db
Ayon <suprovatbit@gmail.com>
2025-03-20 10:37:17 +0600

    Hero UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,27 @@
+//Components
 import BestHotels from "@/components/best-hotels/BestHotels";
 import Hero from "@/components/hero/Hero";
 import PopularLocations from "@/components/popular-locations/PopularLocations";
 
+//image import
+import hotel_image from "../../public/assets/hr_10.jpg";
+import sea from "../../public/assets/sea.jpg";
+
+//Home Page
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Hero
+        image={sea}
+        mainHeader="Are you ready for Adventure?"
+        secondaryHeader="Browse through te popular locations."
+      />
       <PopularLocations />
-      <Hero />
+      <Hero
+        image={hotel_image}
+        mainHeader="Get the best offer for your hotel !"
+        secondaryHeader="Pick your desired place"
+      />
       <BestHotels />
     </>
   );
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,19 @@
-const Hero = () => {
-  return <div>Hero</div>;
+import Image from "next/image";
+
+const Hero = ({ image, mainHeader, secondaryHeader }) => {
+  return (
+    <div>
+      <div className="relative h-screen w-full ">
+        <Image src={image} alt="hero_image" />
+        <div className="absolute bottom-0 left-0 right-0 top-0 flex flex-col justify-center items-center gap-8 ">
+          <h2 className="text-white text-6xl font-bold">{mainHeader}</h2>
+          <h5 className="text-white text-4xl font-semibold">
+            {secondaryHeader}
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
